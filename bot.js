2f03e046546aba597d9038c95436d047d6e4d464
zxhamo <38841829+zxhamo@users.noreply.github.com>
2018-05-25 08:47:14 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,23 +22,7 @@ client.on('message', message => {
     
  
        
-client.on('ready', function(){
-    var ms = 60000 ;
-    var setGame = ['RMDAN KREM','RMDAN KREM','^^help | ^^invite','RMDAN KREM'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/GamerzBot`);
-    }, ms);
 
-});
 
 
 
